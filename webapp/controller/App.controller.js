339d4f2182904986672b7f90926a75df30f7fafc
Ying Heng Tan <yingheng.tan@itelligence.com.my>
2021-12-31 01:27:10 +0000

    Initial Commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("ZYH.ZYH_DIFF.controller.App", {
+		onInit: function () {
+			//test1
+			//test2
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ZYH/ZYH_DIFF/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
